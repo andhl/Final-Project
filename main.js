da8c936126fee43bac30c69398c82e57be2e3d97
andhl <andy.liu.ad21@gmail.com>
2019-02-09 12:23:00 -0500

    added additional apis and added eventlistener for mouse hover

--- main.js
+++ main.js
@@ -43,7 +43,63 @@ function buildLastPrice() {
 }
 
 
+function buildSupply() {
+    fetch("https://api.etherscan.io/api?module=stats&action=ethsupply&apikey=UD6AK73BBJ2GFY9PBFG5XXRXGJ3KDUMQ8X")
+        .then(response => response.json())
+        .then(data => {
+            let supplySpan = document.getElementById('supplynum'),
+                result = data.result / 1000000000000000000;
+            supplySpan.innerHTML = result.toFixed(0);
 
+        })
+
+}
+
+function buildBlock() {
+    fetch("https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=VI286PDTRHXSRUIXED7P1GPGNGBPIF9A4E")
+        .then(response => response.json())
+        .then(data => {
+            let blockSpan = document.getElementById('lastblock'),
+                lastblockresult = data.result;
+            let x = parseInt(lastblockresult, 16)
+            blockSpan.innerHTML = x;
+
+        })
+
+}
+
+async function displayBlueBox() {
+    let [lastPriceJson, totalSuplyJson] = await Promise.all([
+        fetchApi("https://api.etherscan.io/api?module=stats&action=ethprice&apikey=VI286PDTRHXSRUIXED7P1GPGNGBPIF9A4E"),
+        fetchApi("https://api.etherscan.io/api?module=stats&action=ethsupply&apikey=VI286PDTRHXSRUIXED7P1GPGNGBPIF9A4E")
+    ]);
+    let price = lastPriceJson.result.ethusd,
+        totalSupply = totalSuplyJson.result / 1000000000000000000000000000;
+    let mktcap = price * totalSupply;
+    let mktcapSpan = document.getElementById("mktcap");
+    mktcapSpan.innerHTML = mktcap.toFixed(3);
+}
+
+function fetchApi(url) {
+    return fetch(url)
+        .then(response => {
+            return response.json();
+        });
+}
+
+displayBlueBox();
+
+const test = document.querySelector("#Ethsupply");
+
+test.addEventListener("mouseover", function( event ) { 
+    // highlight the mouseover target
+    event.target.style.fontSize = "50px";
+  }, false);
+
+test.addEventListener("mouseout", function( event ) { 
+    // highlight the mouseout target
+    event.target.style.fontSize = "2rem";
+}, false);
 
 
 
